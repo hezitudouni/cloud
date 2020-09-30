f88c602786d76ae745c8b266b0cf903c0a385371
hezitudouni <17611409527@163.com>
2020-09-30 14:01:04 +0800

    测试修改

--- src/main/java/com/hbo/cloud/CloudApplication.java
+++ src/main/java/com/hbo/cloud/CloudApplication.java
@@ -3,16 +3,11 @@ package com.hbo.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 
 @SpringBootApplication
 public class CloudApplication {
 
     public static void main(String[] args) {
-
-
-        System.out.println("args = " + Arrays.deepToString(args));
         SpringApplication.run(CloudApplication.class, args);
     }
 
